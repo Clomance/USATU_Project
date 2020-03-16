5a28ee9fd637e77d0961f32989c1b28e4663c145
Clone <Hisoskar@gmail.com>
2020-03-16 15:19:13 +0300

    Migrating  to AndroidX

--- app/src/main/java/com/example/server_client/SettingsActivity.java
+++ app/src/main/java/com/example/server_client/SettingsActivity.java
@@ -2,11 +2,11 @@ package com.example.server_client;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.design.widget.NavigationView;
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.navigation.NavigationView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
